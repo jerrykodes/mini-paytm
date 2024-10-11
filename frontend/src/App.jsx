09e88e634fa0ddf8c8f41ed9645efe3c278ae48a
Nikhil Gupta <jerrycodes29@gmail.com>
2024-10-11 18:06:06 +0530

    intial fronted setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import SendMoney from './pages/SendMoney'
+import Navbar from './Navbar'
 
-function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+export default function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/' index element={<Home />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/send' element={<SendMoney />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 }
-
-export default App
